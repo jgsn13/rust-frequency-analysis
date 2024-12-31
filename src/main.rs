e3e49b35d26c8f0219525f16a7dfedeca857b4c6
Joaquim Gregório da Silva Neto <joaquim.ibi@gmail.com>
2024-12-31 17:10:02 -0300

    feat: add caesar crack with frequency analysis

--- src/main.rs
+++ src/main.rs
@@ -23,15 +23,36 @@ fn frequency_analysis(text: String) -> Frequency {
     letter_frequencies
 }
 
-fn plot_distribution(frequencies: Frequency) {
+fn plot_distribution(frequency: &Frequency) {
     println!("| {0: <10} | {1: <10} |", "Letter", "Frequency");
-    for (letter, frequency) in frequencies.iter() {
-        println!("| {0: <10} | {1: <10} |", &letter, &frequency);
+    for (letter, freq) in frequency.iter() {
+        println!("| {0: <10} | {1: <10} |", *letter, *freq);
     }
 }
 
+fn caesar_crack(text: String) {
+    let frequency = frequency_analysis(text);
+    plot_distribution(&frequency);
+    let most_frequent = frequency
+        .iter()
+        .reduce(|acc, e| if *e.1 > *acc.1 { e } else { acc })
+        .unwrap();
+    let e_idx = LETTERS.find('E').unwrap();
+    let most_freq_idx = LETTERS
+        .find(*most_frequent.0)
+        .unwrap();
+    let key = most_freq_idx
+        .checked_sub(e_idx)
+        .unwrap_or(LETTERS.len() - e_idx + most_freq_idx);
+    println!("Most frequent letter: {} ({})", most_frequent.0, most_frequent.1);
+    println!("The possible key value: {}", key);
+}
+
 fn main() {
-    let plain_text = String::from("Cryptography is a method of protecting information and communications using codes so that only those for whom the information is intended can read and process it");
-    let frequencies = frequency_analysis(plain_text);
-    plot_distribution(frequencies);
+    let plain_text = String::from("THEHOLYCROSSBEMYLIGHTLETNOTTHEDRAGONBEMYLEADERGETBACKSATANNEVERTEMPTMEWITHVANITYTHEEVILSYOUOFFERYOUSHALLDRINKTHEPOISONYOURSEL");
+    let cipher_text = String::from("WKHKROBFURVVEHPBOLJKWOHWQRWWKHGUDJRQEHPBOHDGHUJHWEDFNVDWDQQHYHUWHPSWPHZLWKYDQLWBWKHHYLOVBRXRIIHUBRXVKDOOGULQNWKHSRLVRQBRXUVHOI");
+    println!("Used key: {}", 3);
+    println!("Decrypted text: {}", plain_text);
+    println!("Encrypter text: {}", cipher_text);
+    caesar_crack(cipher_text);
 }
